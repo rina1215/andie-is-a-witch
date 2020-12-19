9d0a55a10e495085882e7a3e5eeab7128e1fda99
[i2i5] <[i1215@icloud.com]>
2020-12-19 04:13:14 -0500

    make DEBUG configurable

--- bonfire/settings.py
+++ bonfire/settings.py
@@ -32,7 +32,7 @@ MEDIA_URL = '/media/'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
